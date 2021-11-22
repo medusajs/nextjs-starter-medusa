f12d32ab6badb78ba87e422a574fc09bc6e618bb
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-11-22 19:36:31 +0100

    fix: update use of `@medusajs/medusa-js` (#15)
    * fix: Updates use of medusa-js
    
    * small fix
    
    * fix: shipping option retrieval
    
    Co-authored-by: olivermrbl <oliver@mrbltech.com>

--- pages/index.js
+++ pages/index.js
@@ -131,11 +131,11 @@ export default function Home({ products }) {
 
 export const getStaticProps = async () => {
   const client = createClient();
-  const { data } = await client.products.list();
+  const { products } = await client.products.list();
 
   return {
     props: {
-      products: data.products,
+      products,
     },
   };
 };
