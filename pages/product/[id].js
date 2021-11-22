5ce6b7a8210e283bef0f9d9d5ea5d023994da132
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2021-11-22 19:21:43 +0100

    feat: Update use of `@medusa/medusa-js` (#13)
    * fix: Updates use of medusa-js
    
    * small fix
    
    * fix: shipping option retrieval

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -142,8 +142,7 @@ const client = createClient();
 
 export async function getStaticPaths() {
   // Call an external API endpoint to get products
-  const { data } = await client.products.list();
-  const products = data.products;
+  const { products } = await client.products.list();
 
   // Get the paths we want to pre-render based on the products
   const paths = products.map((product) => ({
@@ -159,10 +158,10 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   // params contains the product `id`.
   // If the route is like /product/prod_1, then params.id is 1
-  const { data } = await client.products.retrieve(params.id);
+  const { product } = await client.products.retrieve(params.id);
 
   // Pass post data to the page via props
-  return { props: { product: data.product } };
+  return { props: { product } };
 }
 
 export default Product;
