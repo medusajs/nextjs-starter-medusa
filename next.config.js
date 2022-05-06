94da1e234f89a5d32db8646a1d4411524f510383
Kasper <kasper@medusajs.com>
2022-05-06 15:51:19 +0200

    remove old starter

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
-  },
-};
--- pages/checkout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CheckoutStep from "../components/checkout/checkout-step";
-
-const Checkout = () => {
-  return <CheckoutStep />;
-};
-
-export default Checkout;
