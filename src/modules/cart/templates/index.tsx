d2fd2efc9cea967ba58f1f652564358fa56da8e9
Kasper <kasper@medusajs.com>
2022-07-08 17:38:57 +0200

    added spacing

--- src/modules/cart/templates/index.tsx
+++ src/modules/cart/templates/index.tsx
@@ -21,7 +21,7 @@ const CartTemplate = () => {
       <div className="content-container">
         {cart.items.length ? (
           <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-8">
-            <div className="flex flex-col bg-white p-6">
+            <div className="flex flex-col bg-white p-6 gap-y-6">
               {!customer && <SignInPrompt />}
               <ItemsTemplate region={cart?.region} items={items} />
             </div>
