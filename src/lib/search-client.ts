179e43df8c91e007a6cc2eb3ad51f1534904a6f3
SGF <SGF@sourcegoodfood.com>
2022-07-11 05:59:49 +0000

    feat(algolia search): integrated algolia search

--- src/lib/search-client.ts
+++ src/lib/search-client.ts
@@ -1,4 +1,4 @@
-import { instantMeiliSearch } from "@meilisearch/instant-meilisearch"
+/*import { instantMeiliSearch } from "@meilisearch/instant-meilisearch"
 
 const endpoint =
   process.env.NEXT_PUBLIC_SEARCH_ENDPOINT || "http://127.0.0.1:7700"
@@ -6,6 +6,17 @@ const endpoint =
 const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "test_key"
 
 export const searchClient = instantMeiliSearch(endpoint, apiKey)
+
+export const SEARCH_INDEX_NAME =
+  process.env.NEXT_PUBLIC_INDEX_NAME || "products"*/
+
+import algoliasearch from "algoliasearch/lite"
+
+const appId:string = process.env.NEXT_PUBLIC_SEARCH_APP_ID | "" // You should add this to your environment variables
+
+const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "test_key"
+
+const searchClient = algoliasearch(appId, apiKey)
 
 export const SEARCH_INDEX_NAME =
   process.env.NEXT_PUBLIC_INDEX_NAME || "products"
