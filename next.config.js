c54d1eb32792ed818d14b0be3c8de1a40d847059
SGF <SGF@sourcegoodfood.com>
2022-07-12 06:16:30 +0000

    fix(config update): Updaiting Next.js config for production deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const store = require("./store.config.json")
 module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MEDUSA_BACKEND_URL: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL,
+    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    NEXT_PUBLIC_RAZORPAY_KEY: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
+    NEXT_PUBLIC_SHOP_NAME: process.env.NEXT_PUBLIC_SHOP_NAME,
+    NEXT_PUBLIC_SHOP_DESCRIPTION: process.env.NEXT_PUBLIC_SHOP_DESCRIPTION,
+    NEXT_PUBLIC_CUSTOMER_CARE: process.env.EXT_PUBLIC_CUSTOMER_CARE,
+    NEXT_PUBLIC_FEATURED_PRODUCTS:"Shirt",
+    NEXT_PUBLIC_SEARCH_APP_ID:process.env.NEXT_PUBLIC_SEARCH_APP_ID,
+    NEXT_PUBLIC_SEARCH_API_KEY:process.env.NEXT_PUBLIC_SEARCH_API_KEY,
+  },
   images: {
     domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
   },
