f390146f480e8ae318b1ef3aff2d4ccbc279fa6d
SGF <SGF@sourcegoodfood.com>
2022-07-12 05:49:26 +0000

    fix(export) :incorrect export

--- src/lib/search-client.ts
+++ src/lib/search-client.ts
@@ -16,7 +16,7 @@ const appId = process.env.NEXT_PUBLIC_SEARCH_APP_ID?.toString()?? "" // You shou
 
 const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "test_key"
 
-const searchClient = algoliasearch(appId, apiKey)
+export const searchClient = algoliasearch(appId, apiKey)
 
 export const SEARCH_INDEX_NAME =
   process.env.NEXT_PUBLIC_INDEX_NAME || "products"
