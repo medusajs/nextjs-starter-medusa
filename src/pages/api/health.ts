1ae38c8d232ddb9911e68f34f2526697655ebf55
SGF <SGF@sourcegoodfood.com>
2022-07-14 10:26:19 +0000

    feat(ebs): fixed minor changes necessary to run on EBS

---
+++ src/pages/api/health.ts
@@ -0,0 +1,4 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+export default function handler(req:NextApiRequest, res:NextApiResponse) {
+    res.status(200).json({ "status": "ok" })
+  }
