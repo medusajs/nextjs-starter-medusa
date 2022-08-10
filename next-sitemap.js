7c973abbaf8bc15369f1adec944fbebb1625c2d9
SGF <SGF@sourcegoodfood.com>
2022-08-10 05:14:28 +0000

    updated to support cdn

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 const excludedPaths = ["/checkout", "/account/*"]
 
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
+  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL||process.env.NEXT_PUBLIC_FRONTEND_URL,
   generateRobotsTxt: true,
   exclude: excludedPaths + ["/[sitemap]"],
   robotsTxtOptions: {
