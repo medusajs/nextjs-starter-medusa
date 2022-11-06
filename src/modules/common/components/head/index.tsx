160f767fd3c64d58724ef98058984e69f4227e83
Rafał Petryka <rafalpetryka@gmail.com>
2022-11-06 11:35:03 +0100

    Fix for title warning https://github.com/vercel/next.js/discussions/38256

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -10,7 +10,7 @@ type HeadProps = {
 const Head: React.FC<HeadProps> = ({ title, description, image }) => {
   return (
     <NextHead>
-      <title>{title} | ACME</title>
+      <title>{title + ' | ' + 'ACME'}</title>
       <meta itemProp="name" content={title} />
       {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />}
