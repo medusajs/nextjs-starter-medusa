ceaffa11eec1a21b3e3401f085bd7297602708b8
Kasper <kasper@medusajs.com>
2022-12-08 23:01:34 +0100

    fix: totals of line items depends on item.total

--- src/pages/order/confirmed/[id].tsx
+++ src/pages/order/confirmed/[id].tsx
@@ -4,10 +4,11 @@ import Head from "@modules/common/components/head"
 import Layout from "@modules/layout/templates"
 import OrderCompletedTemplate from "@modules/order/templates/order-completed-template"
 import SkeletonOrderConfirmed from "@modules/skeletons/templates/skeleton-order-confirmed"
+import { useOrder } from "medusa-react"
 import { GetStaticPaths, GetStaticProps } from "next"
 import { useRouter } from "next/router"
 import { ReactElement } from "react"
-import { dehydrate, QueryClient, useQuery } from "react-query"
+import { dehydrate, QueryClient } from "react-query"
 import { NextPageWithLayout } from "types/global"
 
 const fetchOrder = async (id: string) => {
@@ -19,20 +20,16 @@ const Confirmed: NextPageWithLayout = () => {
 
   const id = typeof router.query?.id === "string" ? router.query.id : ""
 
-  const { isSuccess, data, isLoading, isError } = useQuery(
-    ["get_order_confirmed", id],
-    () => fetchOrder(id),
-    {
-      enabled: id.length > 0,
-      staleTime: Infinity,
-    }
-  )
+  const { isSuccess, order, isError, isLoading } = useOrder(id, {
+    enabled: id.length > 0,
+    staleTime: Infinity,
+  })
 
   if (isLoading) {
     return <SkeletonOrderConfirmed />
   }
 
-  if (isError) {
+  if (isError || !order) {
     if (IS_BROWSER) {
       router.replace("/404")
     }
@@ -48,7 +45,7 @@ const Confirmed: NextPageWithLayout = () => {
           description="You purchase was successful"
         />
 
-        <OrderCompletedTemplate order={data} />
+        <OrderCompletedTemplate order={order} />
       </>
     )
   }
