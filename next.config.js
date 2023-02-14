723724c7f4c789bc8c88e7735a91f4aec8185b78
chemicalkosek <chemicalkosek@users.noreply.github.com>
2023-02-14 16:11:07 +0100

    Remove react-query workaround. Bump medusa-react (#108)

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
-const path = require("path")
 
 module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
     domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
-  },
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ["@tanstack/react-query", ...config.externals]
-    }
-    const reactQuery = path.resolve(require.resolve("@tanstack/react-query"))
-    config.resolve.alias["@tanstack/react-query"] = reactQuery
-    return config
   },
 })
 
