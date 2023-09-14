d9d7c860397c861bff0be5f3032920f663c3a0b4
Victor Gerbrands <victorgerbrands@gmail.com>
2023-09-14 18:14:44 +0200

    refactor: product handle route

--- src/app/api/products/[handle]/route.ts
+++ src/app/api/products/[handle]/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse, NextRequest } from "next/server"
-
-import { MedusaApp, Modules } from "@medusajs/modules-sdk"
+import { initialize as initializeProductModule } from "@medusajs/product"
 
 /**
- * This endpoint uses the serverless Product and Pricing Modules to retrieve a product by handle.
+ * This endpoint uses the serverless Product Modules to retrieve a product by handle.
  * The modules connect directly to your Medusa database to retrieve and manipulate data, without the need for a dedicated server.
  * Read more about the Product Module here: https://docs.medusajs.com/modules/products/serverless-module
  */
@@ -14,78 +13,27 @@ export async function GET(
   // Extract the query parameters
   const { handle } = params
 
-  // Initialize Remote Query with the Product and Pricing Modules
-  const { query } = await MedusaApp({
-    modulesConfig: [
-      {
-        module: Modules.PRODUCT,
-        path: "@medusajs/product",
-      },
-      {
-        module: Modules.PRICING,
-        path: "@medusajs/pricing",
-      },
-    ],
-    sharedResourcesConfig: {
-      database: { clientUrl: process.env.POSTGRES_URL },
-    },
-  })
-
-  // Set the filters for the query
-  const filters = {
-    handle,
-    take: 1,
-  }
-
-  // Set the GraphQL query
-  const productsQuery = `#graphql
-    query($handle: String, $take: Int) {
-      products(handle: $handle, take: $take) {
-        id
-        title
-        handle
-        tags
-        status
-        collection
-        collection_id
-        thumbnail
-        images {
-            url
-            alt_text
-            id
-        }
-        options {
-            id
-            value
-            title
-        }
-        variants {
-            id
-            title
-            created_at
-            updated_at
-            thumbnail
-            inventory_quantity
-            material
-            weight
-            length
-            height
-            width
-            options {
-                id
-                value
-                title
-            }
-        }
-      }
-    }`
+  // Initialize the Product Module
+  const productService = await initializeProductModule()
 
   // Run the query
-  const data = await query(productsQuery, filters)
-
-  // Format the response
-  // const products = formatModuleResponse(data)
-  const products = data
+  const products = await productService.list(
+    { handle },
+    {
+      relations: [
+        "variants",
+        "variants.options",
+        "tags",
+        "options",
+        "options.values",
+        "images",
+        "description",
+        "collection",
+        "status",
+      ],
+      take: 1,
+    }
+  )
 
   // Return the response
   return NextResponse.json({ products })
