c4c9b1980aff49ac7908b22c0c3ffc1f692afda4
Victor Gerbrands <victorgerbrands@gmail.com>
2023-11-22 18:09:35 +0000

    fix: image gallery

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -1,60 +1,20 @@
 import { Image as MedusaImage } from "@medusajs/medusa"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
-import { useRef } from "react"
 
 type ImageGalleryProps = {
   images: MedusaImage[]
 }
 
 const ImageGallery = ({ images }: ImageGalleryProps) => {
-  const imageRefs = useRef<(HTMLDivElement | null)[]>([])
-
-  const handleScrollTo = (id: string) => {
-    const element = document.getElementById(id)
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest",
-      })
-    }
-  }
-
   return (
     <div className="flex items-start relative">
-      {/* <div className="hidden small:flex flex-col gap-y-4 sticky top-20">
-        {images.map((image, index) => {
-          return (
-            <button
-              key={image.id}
-              className="h-14 w-12 relative border border-white"
-              onClick={() => {
-                handleScrollTo(image.id)
-              }}
-            >
-              <span className="sr-only">Go to image {index + 1}</span>
-              <Image
-                src={image.url}
-                className="absolute inset-0"
-                alt="Thumbnail"
-                fill
-                sizes="100vw"
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-            </button>
-          )
-        })}
-      </div> */}
       <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
         {images.map((image, index) => {
           return (
             <Container
-              ref={(image) => imageRefs.current.push(image)}
               key={image.id}
-              className="relative aspect-[29/34] w-full overflow-hidden"
+              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
               id={image.id}
             >
               <Image
@@ -63,7 +23,7 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
                 className="absolute inset-0 rounded-rounded"
                 alt={`Product image ${index + 1}`}
                 fill
-                sizes="90vw"
+                sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
                 style={{
                   objectFit: "cover",
                 }}
