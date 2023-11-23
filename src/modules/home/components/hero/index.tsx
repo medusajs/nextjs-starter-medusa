c2d9fe0938b673e51cad512f1ffe3855b49934d3
Victor Gerbrands <victorgerbrands@gmail.com>
2023-11-23 09:38:01 +0000

    fix: button spacing

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,7 +5,7 @@ import { Github } from "@medusajs/icons"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-4">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
