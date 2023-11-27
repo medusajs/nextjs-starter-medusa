8b396649043d9b346675655e1da6943f686787e9
Victor Gerbrands <victorgerbrands@gmail.com>
2023-11-27 13:10:45 +0000

    chore: switch demo to algolia

--- src/app/(main)/search/actions.ts
+++ src/app/(main)/search/actions.ts
@@ -7,18 +7,10 @@ import { searchClient, SEARCH_INDEX_NAME } from "@lib/search-client"
  * @param {string} query - search query
  */
 export async function search(query: string) {
-  // MeiliSearch
-  const queries = [{ params: { query }, indexName: SEARCH_INDEX_NAME }]
-  const { results } = (await searchClient.search(queries)) as Record<
-    string,
-    any
-  >
-  const { hits } = results[0]
-
   // In case you want to use Algolia instead of MeiliSearch, uncomment the following lines and delete the above lines.
 
-  // const index = searchClient.initIndex(SEARCH_INDEX_NAME)
-  // const { hits } = await index.search(query)
+  const index = searchClient.initIndex(SEARCH_INDEX_NAME)
+  const { hits } = await index.search(query)
 
   return hits
 }
