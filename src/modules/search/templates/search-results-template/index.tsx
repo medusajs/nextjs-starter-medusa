3bdc0baf6ba5d3a032e89000dfd919c773b96a4c
Victor Gerbrands <victorgerbrands@gmail.com>
2023-11-27 12:36:39 +0000

    feat: added no results state

--- src/modules/search/templates/search-results-template/index.tsx
+++ src/modules/search/templates/search-results-template/index.tsx
@@ -40,14 +40,20 @@ const SearchResultsTemplate = ({ query, hits }: SearchResultsTemplateProps) => {
         </Link>
       </div>
       <div className="flex flex-col small:flex-row small:items-start py-6">
-        <RefinementList
-          refinementList={params}
-          setRefinementList={setParams}
-          sortBy={sortBy}
-          setSortBy={setSortBy}
-          search
-        />
-        <InfiniteProducts params={params} sortBy={sortBy} />
+        {hits.length > 0 ? (
+          <>
+            <RefinementList
+              refinementList={params}
+              setRefinementList={setParams}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              search
+            />
+            <InfiniteProducts params={params} sortBy={sortBy} />
+          </>
+        ) : (
+          <Text className="ml-8 small:ml-14 mt-3">No results.</Text>
+        )}
       </div>
     </div>
   )
