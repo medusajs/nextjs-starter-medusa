7fad0fdfca74ffca7e61289723dc1564782c2122
Victor Gerbrands <victorgerbrands@gmail.com>
2024-01-09 14:09:39 +0100

    fix: optimize middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,52 +7,52 @@ const DEFAULT_REGION = "us"
 const regionMap = new Map<string, string>()
 
 export async function middleware(request: NextRequest) {
-  const regionCookie = request.cookies.get("region")?.value
+  if (request.cookies.get("region")?.value) return NextResponse.next()
 
-  if (regionCookie) return NextResponse.next()
-
-  const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
-    next: {
-      revalidate: 3600,
-      tags: ["regions"],
-    },
-  }).then((res) => res.json())
-
-  regions.forEach((region: Region) => {
-    const countries = region.countries.map((c) => c.iso_2)
-    countries.forEach((c) => {
-      regionMap.set(c, region.id)
+  try {
+    const res = await fetch(`${BACKEND_URL}/store/regions`, {
+      next: {
+        revalidate: 3600,
+        tags: ["regions"],
+      },
     })
-  })
+    const { regions } = await res.json()
 
-  let regionId = regionMap.get(DEFAULT_REGION)
-  let countryCode = DEFAULT_REGION
+    regions.forEach((region: Region) => {
+      region.countries.forEach((c) => {
+        regionMap.set(c.iso_2, region.id)
+      })
+    })
 
-  const vercelCountry = request.headers
-    .get("x-vercel-ip-country")
-    ?.toLowerCase()
+    let regionId = regionMap.get(DEFAULT_REGION)
+    let countryCode = DEFAULT_REGION
 
-  if (vercelCountry && regionMap.has(vercelCountry)) {
-    regionId = regionMap.get(vercelCountry)
-    countryCode = vercelCountry
+    const vercelCountry = request.headers
+      .get("x-vercel-ip-country")
+      ?.toLowerCase()
+
+    if (vercelCountry && regionMap.has(vercelCountry)) {
+      regionId = regionMap.get(vercelCountry)
+      countryCode = vercelCountry
+    }
+
+    const response = NextResponse.redirect(request.url, 307)
+
+    regionId &&
+      response.cookies.set("region", JSON.stringify({ regionId, countryCode }))
+
+    return response
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") {
+      console.error(
+        "Middleware.ts: Error fetching regions. Did you set up regions in your Medusa Admin and define a NEXT_PUBLIC_MEDUSA_BACKEND_URL environment variable?"
+      )
+    }
+
+    return NextResponse.next()
   }
-
-  const response = NextResponse.redirect(request.url, 307)
-
-  regionId &&
-    response.cookies.set("region", JSON.stringify({ regionId, countryCode }))
-
-  return response
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
 }
