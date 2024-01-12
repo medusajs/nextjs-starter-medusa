f2463563160a30acbf925dd6b3e345bae67c53ab
Victor Gerbrands <victorgerbrands@gmail.com>
2024-01-12 11:17:01 +0100

    fix: reset search to default

--- src/modules/search/actions.ts
+++ src/modules/search/actions.ts
@@ -14,17 +14,17 @@ interface Hits {
  */
 export async function search(query: string) {
   // MeiliSearch
-  // const queries = [{ params: { query }, indexName: SEARCH_INDEX_NAME }]
-  // const { results } = (await searchClient.search(queries)) as Record<
-  //   string,
-  //   any
-  // >
-  // const { hits } = results[0] as { hits: Hits[] }
+  const queries = [{ params: { query }, indexName: SEARCH_INDEX_NAME }]
+  const { results } = (await searchClient.search(queries)) as Record<
+    string,
+    any
+  >
+  const { hits } = results[0] as { hits: Hits[] }
 
   // In case you want to use Algolia instead of MeiliSearch, uncomment the following lines and delete the above lines.
 
-  const index = searchClient.initIndex(SEARCH_INDEX_NAME)
-  const { hits } = (await index.search(query)) as { hits: Hits[] }
+  // const index = searchClient.initIndex(SEARCH_INDEX_NAME)
+  // const { hits } = (await index.search(query)) as { hits: Hits[] }
 
   return hits
 }
