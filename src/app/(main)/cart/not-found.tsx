8bf380b423245b3a35a4c17a2487b398c61db14a
Victor Gerbrands <victorgerbrands@gmail.com>
2024-01-12 11:01:24 +0100

    fix: not-found classname

--- src/app/(main)/cart/not-found.tsx
+++ src/app/(main)/cart/not-found.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-gry-900">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
       <p className="text-small-regular text-ui-fg-base">
         The cart you tried to access does not exist. Clear your cookies and try
         again.
