8179f757a31ca9936e869de9f3b651fc1baf9515
Victor Gerbrands <victorgerbrands@gmail.com>
2024-01-12 12:20:24 +0100

    fix: logout

--- src/app/(main)/account/@dashboard/loading.tsx
+++ src/app/(main)/account/@dashboard/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@modules/common/icons/spinner"
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center w-full min-h-[640px] h-full text-ui-fg-base">
+    <div className="flex items-center justify-center w-full h-full text-ui-fg-base">
       <Spinner size={36} />
     </div>
   )
