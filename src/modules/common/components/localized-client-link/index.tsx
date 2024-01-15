f385cf4a2ad869299d426da2fe9cd54760ad1dd4
Victor Gerbrands <victorgerbrands@gmail.com>
2024-01-15 14:43:35 +0100

    fix: cleanup, note

--- src/modules/common/components/localized-client-link/index.tsx
+++ src/modules/common/components/localized-client-link/index.tsx
@@ -4,6 +4,10 @@ import Link from "next/link"
 import { useParams } from "next/navigation"
 import React from "react"
 
+/**
+ * Use this component to create a Next.js `<Link />` that persists the current country code in the url,
+ * without having to explicitly pass it as a prop.
+ */
 const LocalizedClientLink = ({
   children,
   href,
