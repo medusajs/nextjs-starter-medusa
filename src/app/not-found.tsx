dceac1e81af8f57ec0e02dab5b0e13ac418b40f0
festelle <62019727+festelle@users.noreply.github.com>
2024-01-25 11:46:24 -0300

    Fix: not-found link to frontpage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next"
 
-import InteractiveLink from "@modules/common/components/interactive-link"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "404",
@@ -14,7 +14,7 @@ export default function NotFound() {
       <p className="text-small-regular text-ui-fg-base">
         The page you tried to access does not exist.
       </p>
-      <InteractiveLink href="/">Go to frontpage</InteractiveLink>
+      <Link href="/">Go to frontpage</Link>
     </div>
   )
 }
