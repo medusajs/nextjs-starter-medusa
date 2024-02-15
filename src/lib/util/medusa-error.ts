8d71fc38941e671393075d31622b6981389af919
QAComet <lucas@qacomet.com>
2024-02-15 16:50:02 -0700

    feat: Improve error reporting by adding resource

--- src/lib/util/medusa-error.ts
+++ src/lib/util/medusa-error.ts
@@ -2,6 +2,8 @@ export default function medusaError(error: any) {
   if (error.response) {
     // The request was made and the server responded with a status code
     // that falls out of the range of 2xx
+    const u = new URL(error.config.url, error.config.baseURL)
+    console.error("Resource:", u.toString())
     console.error("Response data:", error.response.data)
     console.error("Status code:", error.response.status)
     console.error("Headers:", error.response.headers)
