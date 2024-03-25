936ef0546552476a7f2fcf6c38c3c7a071d8115f
QAComet <lucas@qacomet.com>
2024-03-25 14:41:53 -0600

    feat(action): write github action for running tests on new PR's

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'npm run start',
+     command: 'yarn start',
      url: process.env.NEXT_PUBLIC_BASE_URL,
   //   reuseExistingServer: !process.env.CI,
   },
