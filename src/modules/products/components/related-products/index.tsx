37519881a882bbe7ea741b2e78b2abe3db963a24
olivermrbl <oliver@mrbltech.com>
2024-07-03 14:39:36 +0200

    fix: Minor product type issues

--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -19,7 +19,7 @@ export default async function RelatedProducts({
   }
 
   // edit this function to define your related products logic
-  const queryParams: HttpTypes.StoreProductFilters = {}
+  const queryParams: HttpTypes.StoreProductParams = {}
   if (region?.id) {
     queryParams.region_id = region.id
   }
