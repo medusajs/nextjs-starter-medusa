f5491281db9ed26ee91d3a4f751d0d50185d84b5
Victor Gerbrands <victorgerbrands@gmail.com>
2024-07-04 16:48:10 +0200

    cleanup

--- src/modules/store/components/refinement-list/sort-products/index.tsx
+++ src/modules/store/components/refinement-list/sort-products/index.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { ChangeEvent } from "react"
-
 import FilterRadioGroup from "@modules/common/components/filter-radio-group"
 
 export type SortOptions = "price_asc" | "price_desc" | "created_at"
