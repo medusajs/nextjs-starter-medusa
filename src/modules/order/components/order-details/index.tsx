e81c05aba94401533b5784b759aaa529b1170c88
mandy <mandyhale10@gmail.com>
2024-08-12 10:11:13 +1200

    one place to manage margin

--- src/modules/order/components/order-details/index.tsx
+++ src/modules/order/components/order-details/index.tsx
@@ -15,19 +15,21 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
 
   return (
     <div>
-      <Text>
-        We have sent the order confirmation details to{" "}
-        <span className="font-semibold text-ui-fg-medium-plus" data-testid="order-email">
-          {order.email}
-        </span>
-        .
-      </Text>
-      <Text className="mt-2">
-        Order date: <span data-testid="order-date">{new Date(order.created_at).toDateString()}</span>
-      </Text>
-      <Text className="mt-2 text-ui-fg-interactive">
-        Order number <span data-testid="order-id">{order.display_id}</span>
-      </Text>
+        <div className="flex flex-col gap-y-2">
+          <Text>
+            We have sent the order confirmation details to{" "}
+            <span className="font-semibold text-ui-fg-medium-plus" data-testid="order-email">
+              {order.email}
+            </span>
+            .
+          </Text>
+          <Text>
+            Order date: <span data-testid="order-date">{new Date(order.created_at).toDateString()}</span>
+          </Text>
+          <Text className="text-ui-fg-interactive">
+            Order number <span data-testid="order-id">{order.display_id}</span>
+          </Text>
+        </div>
 
 {/* TODO check if we want to pass the showStatus prop to show the customer order status or payment status details? */}
       <div className="flex items-center mt-4 text-compact-small gap-x-4">
