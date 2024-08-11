c1510d7a2ff727df437adc650eb2634eb6911de3
mandy <mandyhale10@gmail.com>
2024-08-12 11:09:29 +1200

    extran bank payment info to component and tidy up comments

--- src/modules/order/components/order-confirmed/index.tsx
+++ src/modules/order/components/order-confirmed/index.tsx
@@ -5,6 +5,7 @@ import OrderDetails from "../order-details"
 import PickUpInfo from "../pick-up-info"
 
 import { useEffect, useState } from "react"
+import BankPaymentInstructions from "../bank-payment-instructions"
 
 type OrderConfirmedProps = {
   order: Order
@@ -13,18 +14,15 @@ type OrderConfirmedProps = {
 const OrderConfirmed = ({ order}: OrderConfirmedProps) => {
   
   const [isManualPayment, setIsManualPayment] = useState<boolean | undefined>(undefined);
-  // const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setIsManualPayment(order.payments[0].provider_id === "manual");
-    // setIsLoading(false)
   }, [order.payments]);
 
 
 console.log('provider_id:', order.payments[0].provider_id);
 console.log('Condition result:', order.payments[0].provider_id !== "manual");
 console.log('isManualPayment:', isManualPayment);
-// console.log(isLoading)
 
 if (isManualPayment === undefined) {
   return (
@@ -37,33 +35,16 @@ if (isManualPayment === undefined) {
 if (isManualPayment) {
   return (
     <>
-      <Heading
-            level="h1"
-            className="flex flex-col mb-3 text-3xl gap-y-3 text-ui-fg-base"
-          >
-            <span>Almost there!</span>
-            <span>😊 🥦</span>
-          </Heading>
-          <Heading level="h2" className="text-2xl-regular">
-            Bank payment instructions
-          </Heading>
-          <Text>
-          Thanks for using our platform! You are one step away from completing your order! 
-          The final step is to manually transfer us the money. You can find the transfer details below. 
-          Please put the <span className="font-semibold">order number #<span data-testid="order-id">{order.display_id}</span></span> in the reference field.
-          </Text>
-          <Text>
-          </Text>
-          <div className="flex flex-col space-y-2">
-            <Heading level="h3" className="text-xl-regular">Payment details</Heading>
-            <Text><span className="font-semibold">Bank account name:</span> CoShop</Text>
-            <Text><span className="font-semibold">Account number:</span> 1234 5678 9012</Text>
-            <Text><span className="font-semibold">Reference:</span> {order.display_id}</Text>
-            {/* TODO format the total amount */}
-            <Text><span className="font-semibold">Amount due (incl tax):</span> {order.total}</Text> 
-          <Text>NOTE: Your order is not ready until you have paid the amount due.</Text>
-          <Text>If you have any questions or feedback, reach out to <a className="underline" href="mailto:orders@coshop.nz">orders@coshop.nz</a></Text>
-          </div>
+      <div className="flex flex-col space-y-2">
+        <Heading
+          level="h1"
+          className="flex flex-col mb-3 text-3xl gap-y-3 text-ui-fg-base"
+        >
+          <span>Almost there!</span>
+          <span>😊 🥦</span>
+        </Heading>
+        <BankPaymentInstructions order={order} />
+      </div>
     </>
   );
 }
