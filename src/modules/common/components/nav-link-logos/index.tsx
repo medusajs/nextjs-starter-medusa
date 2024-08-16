c71152db96bcc7928966a5f182970033a90d5b27
mandy <mandyhale10@gmail.com>
2024-08-16 20:57:32 +1200

    add in coshop logos and links

--- src/modules/common/components/nav-link-logos/index.tsx
+++ src/modules/common/components/nav-link-logos/index.tsx
@@ -2,21 +2,24 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import Image from "next/image"
 
 const NavLinkLogos = () => (
-  <LocalizedClientLink
-    href="/products/mixed-bag"
-    className="flex flex-row items-center uppercase gap-x-2 txt-compact-xlarge-plus hover:text-ui-fg-base"
-    data-testid="nav-store-link"
-  >
-    <div className="flex flex-row items-center gap-x-2">
-      <Image src="/coshop-logo.png" alt="" width={32} height={32} />
-      <span>CoShop</span>
-    </div>
-    <span>&</span>
-    <div className="flex flex-row items-center gap-x-2">
-      <span>Hauora Kai Karori</span>
-      <Image src="/coop-img.jpg" alt="" width={32} height={32} />
-    </div>
-  </LocalizedClientLink>
+        <div>
+          <LocalizedClientLink
+            href="/products/mixed-bag"
+            className="flex flex-row items-center h-full uppercase gap-x-2 txt-compact-large-plus sm:txt-compact-xlarge-plus hover:text-ui-fg-base"
+            data-testid="nav-store-link"
+          >
+            {/* TODO delete coshop from header if not required */}
+            {/* <div className="flex flex-row items-center gap-x-1">
+              <Image src="/coshop-logo.png" alt="" width={20} height={20} />
+              <span>CoShop</span>
+            </div>
+            <span>&</span> */}
+            <div className="flex flex-row items-center gap-x-1">
+              <Image src="/coop-img.jpg" alt="" width={20} height={20} />
+              <span>Hauora Kai Karori</span>
+            </div>
+          </LocalizedClientLink>
+        </div>
 )
 
 export default NavLinkLogos
