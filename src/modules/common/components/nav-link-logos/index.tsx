579cf0ca96a36f5dce9e1a9d4c3829764620a4e8
mandy <mandyhale10@gmail.com>
2024-08-16 16:43:00 +1200

    dont need alt text on decorative images

--- src/modules/common/components/nav-link-logos/index.tsx
+++ src/modules/common/components/nav-link-logos/index.tsx
@@ -8,13 +8,13 @@ const NavLinkLogos = () => (
     data-testid="nav-store-link"
   >
     <div className="flex flex-row items-center gap-x-2">
-      <Image src="/coshop-logo.png" alt="logo" width={32} height={32} />
+      <Image src="/coshop-logo.png" alt="" width={32} height={32} />
       <span>CoShop</span>
     </div>
     <span>&</span>
     <div className="flex flex-row items-center gap-x-2">
       <span>Hauora Kai Karori</span>
-      <Image src="/coop-img.jpg" alt="logo" width={32} height={32} />
+      <Image src="/coop-img.jpg" alt="" width={32} height={32} />
     </div>
   </LocalizedClientLink>
 )
