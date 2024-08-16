e97c6d1d6a8d35621923e11df97c1de94afabb96
mandy <mandyhale10@gmail.com>
2024-08-16 20:54:36 +1200

    New footer copy and logo

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -3,30 +3,32 @@ import { Text, clx } from "@medusajs/ui"
 import { getCategoriesList, getCollectionsList } from "@lib/data"
 
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import MedusaCTA from "@modules/layout/components/medusa-cta"
+import PoweredByLogos from "@modules/layout/components/powered-by-logos"
+import Image from "next/image"
 
 export default async function Footer() {
   const { collections } = await getCollectionsList(0, 6)
   const { product_categories } = await getCategoriesList(0, 6)
 
   return (
-    <footer className="border-t border-ui-border-base w-full">
-      <div className="content-container flex flex-col w-full">
-        <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between py-10">
+    <footer className="w-full border-t border-ui-border-base">
+      <div className="flex flex-col w-full content-container">
+        <div className="flex flex-col items-start justify-between py-10 gap-y-6 xsmall:flex-row">
           <div>
             <LocalizedClientLink
               href="/"
-              className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
+              className="flex items-center uppercase gap-x-2 txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base"
             >
-              Powered by CoShop
+              <Image src="/coshop-logo.png" alt="" width={32} height={32} />
+              <span>Powered by CoShop</span>
             </LocalizedClientLink>
           </div>
         </div>
-        <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
+        <div className="flex justify-between w-full mb-16 text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Powered by CoShop™
+            © {new Date().getFullYear()} CoShop™
           </Text>
-          {/* <MedusaCTA /> */}
+          {/* <PoweredByLogos /> */}
         </div>
       </div>
     </footer>
