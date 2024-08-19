7b74b31cfe15fdea4baa85edbbd4c43013d31f9b
Ira <ira@ira.nz>
2024-08-19 14:02:35 +1200

    Add Shopping Cart Icon

--- src/modules/layout/components/cart-button/index.tsx
+++ src/modules/layout/components/cart-button/index.tsx
@@ -1,7 +1,8 @@
 import { LineItem } from "@medusajs/medusa"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
 import { enrichLineItems, retrieveCart } from "@modules/cart/actions"
-
+import { BsCart } from 'react-icons/bs'
 import CartDropdown from "../cart-dropdown"
 
 const fetchCart = async () => {
@@ -10,6 +11,7 @@ const fetchCart = async () => {
   if (cart?.items.length) {
     const enrichedItems = await enrichLineItems(cart?.items, cart?.region_id)
     cart.items = enrichedItems as LineItem[]
+    
   }
 
   return cart
@@ -18,5 +20,12 @@ const fetchCart = async () => {
 export default async function CartButton() {
   const cart = await fetchCart()
 
-  return <CartDropdown cart={cart} />
+  return (
+    <div className="flex items-center"> 
+      <CartDropdown cart={cart} />
+      <LocalizedClientLink href="/cart" className="ml-2 mr-4">
+        <BsCart className="h-5 w-5" /> 
+      </LocalizedClientLink>
+    </div>
+  );
 }
