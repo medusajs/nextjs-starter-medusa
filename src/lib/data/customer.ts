80e94e56f68882de75b890fc3c9a5ca53015a16b
olivermrbl <oliver@mrbltech.com>
2024-09-04 10:45:31 +0200

    fix: Sign up

--- src/lib/data/customer.ts
+++ src/lib/data/customer.ts
@@ -37,12 +37,13 @@ export async function signup(_currentState: unknown, formData: FormData) {
   }
 
   try {
-    const { token } = await sdk.auth.create("customer", "emailpass", {
+    const token = await sdk.auth.register("customer", "emailpass", {
       email: customerForm.email,
       password: password,
     })
 
     const customHeaders = { authorization: `Bearer ${token}` }
+    
     const { customer: createdCustomer } = await sdk.store.customer.create(
       customerForm,
       {},
