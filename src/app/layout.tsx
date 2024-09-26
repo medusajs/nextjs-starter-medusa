c97908b6acc16ca278fc8e425022a35e220d9515
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-09-26 19:26:49 +0200

    Fixed lint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   width: 'device-width',
   maximumScale: 1.0,
-  userScalable: 'no'
+  userScalable: false
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
