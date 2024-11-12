5e9928001a9f63c82be2828d4b102079608277e6
ouestlabs <contact.ouest.labs@gmail.com>
2024-11-12 17:02:34 +0000

    fix: Force products page dynamic rendering

--- src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -10,6 +10,8 @@ type Props = {
   params: { countryCode: string; handle: string }
 }
 
+export const dynamic = 'force-dynamic'
+
 export async function generateStaticParams() {
   try {
     const countryCodes = await listRegions().then((regions) =>
