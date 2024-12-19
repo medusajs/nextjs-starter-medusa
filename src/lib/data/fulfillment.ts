4b1691e8a0e57bcc7b612ca48e57672244050f3d
Frane Polić <16856471+fPolic@users.noreply.github.com>
2024-12-19 08:22:20 +0100

    feat: support calculated SO at checkout (#436)
    * feat: support calculated SO at checkout
    
    * fix: address feedback
    
    * fix: disable selecting option without calcualted price

--- src/lib/data/fulfillment.ts
+++ src/lib/data/fulfillment.ts
@@ -29,3 +29,38 @@ export const listCartShippingMethods = async (cartId: string) => {
       return null
     })
 }
+
+export const calculatePriceForShippingOption = async (
+  optionId: string,
+  cartId: string,
+  data?: Record<string, unknown>
+) => {
+  const headers = {
+    ...(await getAuthHeaders()),
+  }
+
+  const next = {
+    ...(await getCacheOptions("fulfillment")),
+  }
+
+  const body = { cart_id: cartId, data }
+
+  if (data) {
+    body.data = data
+  }
+
+  return sdk.client
+    .fetch<{ shipping_option: HttpTypes.StoreCartShippingOption }>(
+      `/store/shipping-options/${optionId}/calculate`,
+      {
+        method: "POST",
+        body,
+        headers,
+        next,
+      }
+    )
+    .then(({ shipping_option }) => shipping_option)
+    .catch((e) => {
+      return null
+    })
+}
