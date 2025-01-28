ad6d27572f75a2d440d479f515616e3e88cf8e98
shaheer <sh@sheenapps.com>
2025-01-28 17:04:52 +0200

    use createNavigation() instead of deprecated createSharedPathnamesNavigation

--- src/lib/i18n/navigation.ts
+++ src/lib/i18n/navigation.ts
@@ -1,5 +1,5 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation"
+import { createNavigation } from "next-intl/navigation"
 import { routing } from "./settings"
 
 export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation(routing)
+  createNavigation(routing)
