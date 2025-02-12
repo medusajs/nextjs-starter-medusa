7f0f063264a12afffb126c42be7f359118fd302b
Tomer Yechiel <32360493+tomer-yechiel@users.noreply.github.com>
2025-02-12 07:23:20 +0000

    fix text description

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -8,7 +8,7 @@ import { getRegion } from "@lib/data/regions"
 export const metadata: Metadata = {
   title: "Medusa Next.js Starter Template",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
 }
 
 export default async function Home(props: {
