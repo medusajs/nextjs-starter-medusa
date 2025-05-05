cb2d68a794e864172f9778296ebb9af153fe2f7a
Shahed Nasser <shahednasser@gmail.com>
2025-05-05 14:09:57 +0300

    fix: show order's payment and fulfillment status

--- src/modules/order/components/order-details/index.tsx
+++ src/modules/order/components/order-details/index.tsx
@@ -42,7 +42,7 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
               Order status:{" "}
               <span className="text-ui-fg-subtle " data-testid="order-status">
                 {/* TODO: Check where the statuses should come from */}
-                {/* {formatStatus(order.fulfillment_status)} */}
+                {formatStatus(order.fulfillment_status)}
               </span>
             </Text>
             <Text>
@@ -51,7 +51,7 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
                 className="text-ui-fg-subtle "
                 sata-testid="order-payment-status"
               >
-                {/* {formatStatus(order.payment_status)} */}
+                {formatStatus(order.payment_status)}
               </span>
             </Text>
           </>
