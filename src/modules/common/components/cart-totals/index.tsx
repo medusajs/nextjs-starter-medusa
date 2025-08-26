0ad5a8feb95413c453fd5a841985241cf14bd305
William Bouchard <46496014+willbouch@users.noreply.github.com>
2025-08-26 10:44:19 -0400

    feat(): handle free shipping in cart display (#508)
    * feat(): handle free shipping in cart display
    
    * remove console log

--- src/modules/common/components/cart-totals/index.tsx
+++ src/modules/common/components/cart-totals/index.tsx
@@ -2,6 +2,7 @@
 
 import { convertToLocale } from "@lib/util/money"
 import React from "react"
+import { Badge } from "@medusajs/ui"
 
 type CartTotalsProps = {
   totals: {
@@ -12,7 +13,11 @@ type CartTotalsProps = {
     discount_total?: number | null
     gift_card_total?: number | null
     currency_code: string
+    item_subtotal?: number | null
+    item_total?: number | null
     shipping_subtotal?: number | null
+    shipping_methods?: {adjustments?: {amount: number}[]}[]
+    items?: {adjustments?: {amount: number}[]}[]
   }
 }
 
@@ -20,42 +25,57 @@ const CartTotals: React.FC<CartTotalsProps> = ({ totals }) => {
   const {
     currency_code,
     total,
-    subtotal,
     tax_total,
-    discount_total,
     gift_card_total,
+    item_subtotal,
+    item_total,
     shipping_subtotal,
+    shipping_total,
+    shipping_methods,
+    items
   } = totals
+
+  const totalItemDiscount = items?.flatMap(i => i.adjustments).reduce((acc, curr) => (curr?.amount || 0) + acc, 0)
+  const totalShippingDiscount = shipping_methods?.flatMap(sm => sm.adjustments).reduce((acc, curr) => (curr?.amount || 0) + acc, 0)
 
   return (
     <div>
       <div className="flex flex-col gap-y-2 txt-medium text-ui-fg-subtle ">
         <div className="flex items-center justify-between">
-          <span className="flex gap-x-1 items-center">
-            Subtotal (excl. shipping and taxes)
-          </span>
-          <span data-testid="cart-subtotal" data-value={subtotal || 0}>
-            {convertToLocale({ amount: subtotal ?? 0, currency_code })}
-          </span>
-        </div>
-        {!!discount_total && (
-          <div className="flex items-center justify-between">
-            <span>Discount</span>
+          <span>Subtotal (excl. shipping and taxes)</span>
+          <div className="flex items-center gap-2">
+            <span className={totalItemDiscount ? 'line-through text-ui-fg-muted' : ''} data-testid="cart-item-subtotal" data-value={item_subtotal || 0}>
+              {convertToLocale({ amount: item_subtotal ?? 0, currency_code })}
+            </span>
+            {!!totalItemDiscount && (
+              <div className="flex items-center justify-between">
             <span
               className="text-ui-fg-interactive"
               data-testid="cart-discount"
-              data-value={discount_total || 0}
+              data-value={item_total || 0}
             >
-              -{" "}
-              {convertToLocale({ amount: discount_total ?? 0, currency_code })}
+              {convertToLocale({ amount: item_total ?? 0, currency_code })}
             </span>
+              </div>
+            )}
           </div>
-        )}
+        </div>
         <div className="flex items-center justify-between">
           <span>Shipping</span>
-          <span data-testid="cart-shipping" data-value={shipping_subtotal || 0}>
-            {convertToLocale({ amount: shipping_subtotal ?? 0, currency_code })}
-          </span>
+          <div className="flex items-center gap-2">
+            <span className={totalShippingDiscount ? 'line-through text-ui-fg-muted' : ''} data-testid="cart-shipping" data-value={shipping_subtotal || 0}>
+              {convertToLocale({ amount: shipping_subtotal ?? 0, currency_code })}
+            </span>
+            {!!totalShippingDiscount && (
+              <span
+                className="text-ui-fg-interactive"
+                data-testid="cart-shipping-discount"
+                data-value={shipping_total || 0}
+              >
+              {convertToLocale({ amount: shipping_total ?? 0, currency_code })}
+            </span>
+            )}
+          </div>
         </div>
         <div className="flex justify-between">
           <span className="flex gap-x-1 items-center ">Taxes</span>
