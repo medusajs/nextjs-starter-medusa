013df88878ecfc400356a7daa77c78988be88448
Shahed Nasser <shahednasser@gmail.com>
2025-10-09 16:01:31 +0300

    Add new S3 hostname configuration for cloud

--- next.config.js
+++ next.config.js
@@ -36,6 +36,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "s3.eu-central-1.amazonaws.com",
+        pathname: "medusajs.cloud-data-prod-**/**"
     ],
   },
 }
