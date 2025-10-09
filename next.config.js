66f8feeef2443e885cf6de4f4a1f8b15d832a5eb
Shahed Nasser <shahednasser@gmail.com>
2025-10-09 17:32:08 +0300

    feat: add environment variables for S3 setup on Cloud (#526)
    * Add new S3 hostname configuration for cloud
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Fix pathname in next.config.js for S3
    
    * added medusa cloud config
    
    * fix pathname

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const checkEnvVariables = require("./check-env-variables")
 checkEnvVariables()
 
 /**
+ * Medusa Cloud-related environment variables
+ */
+const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME
+const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
+
+/**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
@@ -36,6 +42,15 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      ...(S3_HOSTNAME && S3_PATHNAME
+        ? [
+            {
+              protocol: "https",
+              hostname: S3_HOSTNAME,
+              pathname: S3_PATHNAME,
+            },
+          ]
+        : []),
     ],
   },
 }
