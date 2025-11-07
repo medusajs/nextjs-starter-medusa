c70162e6fab5390f0ac731517fa71bb5964d1db1
Nicolas Gorga <nicogorga11@gmail.com>
2025-11-07 15:44:40 -0300

    Add backdrop

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -34,6 +34,14 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 </Popover.Button>
               </div>
 
+              {open && (
+                <div
+                  className="fixed inset-0 z-[50] bg-black/0 pointer-events-auto"
+                  onClick={close}
+                  data-testid="side-menu-backdrop"
+                />
+              )}
+
               <Transition
                 show={open}
                 as={Fragment}
@@ -44,7 +52,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 leaveFrom="opacity-100 backdrop-blur-2xl"
                 leaveTo="opacity-0"
               >
-                <PopoverPanel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
+                <PopoverPanel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-[51] inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
                   <div
                     data-testid="nav-menu-popup"
                     className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
